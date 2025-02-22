4de7edbb812004c9726b9f718595a1860821a784
Sean Andres <133720513+sdandres@users.noreply.github.com>
2025-02-21 19:52:55 -0800

    v0.0.3 - added customer object
    - added customer object logic, base file folder
    
    Signed-off-by: Sean Andres <133720513+sdandres@users.noreply.github.com>

---
+++ draft_build_0.1/customer object/build/customer_object.py
@@ -0,0 +1,14 @@
+class customer_object:
+    def __init__ (self, customerID, first_name, last_name, email, phone_number, address, address2, city, state, zipcode):
+        self.customerID = customerID
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+        self.address2 = address2
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+    
+    def generate_customerID ():
