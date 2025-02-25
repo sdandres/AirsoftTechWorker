b9995334dc1381c53801e86f5316277fdc105f8e
Sean Andres <133720513+sdandres@users.noreply.github.com>
2025-02-25 14:35:29 -0800

    Update customer_object.py
    Signed-off-by: Sean Andres <133720513+sdandres@users.noreply.github.com>

--- draft_build_0.1/objects/build/customer_object.py
+++ draft_build_0.1/objects/build/customer_object.py
@@ -11,4 +11,20 @@ class customer_object:
         self.state = state
         self.zipcode = zipcode
     
-    def generate_customerID ():
+    def to_dict(self):
+        return {
+            "Customer ID": self.customerID,
+            "First Name": self.first_name,
+            "Last Name": self.last_name,
+            "Email": self.email,
+            "Phone Number": self.phone_number,
+            "Address": self.address,
+            "Address 2": self.address2,
+            "City": self.city,
+            "State": self.state,
+            "Zipcode": self.zipcode
+        }
+
+def save_data(self, customer_object):
+    df = pd.DataFrame([self.to_dict()])
+    df.to_csv('customer_data.csv', mode='a', index=False, encoding='utf-8', header=not pd.io.common.file_exists('customer_data.csv'))
