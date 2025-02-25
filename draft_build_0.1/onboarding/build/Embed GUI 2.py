38a06ab3a28c33587449ceefb20cdbc776a3328a
mpdodds <madlynedodds@csus.edu>
2025-02-25 13:17:32 -0800

    Update Embed GUI 2.py
    Removed class CustomerObject
    Added from customer import CustomerObject
    
    Signed-off-by: mpdodds <madlynedodds@csus.edu>

--- draft_build_0.1/onboarding/build/Embed GUI 2.py
+++ draft_build_0.1/onboarding/build/Embed GUI 2.py
@@ -4,6 +4,7 @@ from tkinter import ttk  # for Combobox and Entry
 import pandas as pd
 import re
 from pathlib import Path
+from customer import CustomerObject
 
 # ================
 # DESIGNER SECTION
@@ -197,34 +198,6 @@ def autocomplete(event):
         state_combobox['values'] = states
 
 state_combobox.bind('<KeyRelease>', autocomplete)
-
-class CustomerObject:
-    def __init__(self, customerID, first_name, last_name, email,
-                 phone_number, address, address2, city, state, zipcode):
-        self.customerID = customerID
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.phone_number = phone_number
-        self.address = address
-        self.address2 = address2
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-
-    def to_dict(self):
-        return {
-            "Customer ID": self.customerID,
-            "First Name": self.first_name,
-            "Last Name": self.last_name,
-            "Email": self.email,
-            "Phone Number": self.phone_number,
-            "Address": self.address,
-            "Address 2": self.address2,
-            "City": self.city,
-            "State": self.state,
-            "Zipcode": self.zipcode
-        }
 
 def generate_customer_id(last_name, mobile):
     last_name_part = last_name[:3].upper() if last_name else "XXX"
