f2700f0d34996d98704800741d5c37125edd4450
Sean Andres <133720513+sdandres@users.noreply.github.com>
2025-05-05 17:37:44 -0700

    Delete main.py
    Signed-off-by: Sean Andres <133720513+sdandres@users.noreply.github.com>

--- main.py
+++
@@ -1,9 +0,0 @@
-import gui.gui as gui
-from tkinter import Tk
-
-if __name__ == "__main__":
-    window = Tk()
-
-    window.geometry("1200x720")
-    window.configure(bg = "#FFFFFF")
-    gui.open_login_page(window)
